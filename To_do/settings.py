8f4b8b43b038dabd912f1baad393b18434a152f2
DakshDudeja <61489137+DakshDudeja@users.noreply.github.com>
2021-06-12 00:29:47 +0530

    Update settings.py

--- To_do/settings.py
+++ To_do/settings.py
@@ -123,4 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 django_heroku.settings(locals())
